5b90c823b26236489031c08008e2d6c5ebef7987
KosmX <kosmx.mc@gmail.com>
2022-07-08 01:24:46 +0200

    some modifiers

---
+++ coreLib/src/main/java/dev/kosmx/playerAnim/api/layered/MirrorModifier.java
@@ -0,0 +1,55 @@
+package dev.kosmx.playerAnim.api.layered;
+
+import dev.kosmx.playerAnim.api.TransformType;
+import dev.kosmx.playerAnim.core.util.Vec3f;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MirrorModifier extends IModifier {
+
+    public static final Map<String, String> mirrorMap;
+
+    /**
+     * Enable the modifier
+     */
+    @Getter
+    @Setter
+    private boolean enabled = true;
+
+    @Override
+    public Vec3f get3DTransform(String modelName, TransformType type, float tickDelta, Vec3f value0) {
+        if (!enabled) return super.get3DTransform(modelName, type, tickDelta, value0);
+
+        if (mirrorMap.containsKey(modelName)) modelName = mirrorMap.get(modelName);
+        value0 = transformVector(value0, type);
+
+        Vec3f vec3f = super.get3DTransform(modelName, type, tickDelta, value0);
+        return transformVector(vec3f, type);
+    }
+
+    protected Vec3f transformVector(Vec3f value0, TransformType type) {
+        switch (type) {
+            case POSITION:
+                return new Vec3f(-value0.getX(), value0.getY(), value0.getZ());
+            case ROTATION:
+                return new Vec3f(value0.getX(), -value0.getY(), -value0.getZ());
+            case BEND:
+                return new Vec3f(value0.getX(), -value0.getY(), value0.getZ());
+            default:
+                return value0; //why?!
+        }
+    }
+
+    static {
+        HashMap<String, String> partMap = new HashMap<>();
+        partMap.put("leftArm", "rightArm");
+        partMap.put("leftLeg", "rightLeg");
+        partMap.put("rightArm", "leftArm");
+        partMap.put("rightLeg", "leftLeg");
+        mirrorMap = Collections.unmodifiableMap(partMap);
+    }
+}
---
+++ coreLib/src/main/java/dev/kosmx/playerAnim/api/layered/SpeedModifier.java
@@ -0,0 +1,46 @@
+package dev.kosmx.playerAnim.api.layered;
+
+import dev.kosmx.playerAnim.api.TransformType;
+import dev.kosmx.playerAnim.core.util.Vec3f;
+
+/**
+ * Modifies the animation speed.
+ * speed = 2 means twice the speed, the animation will take half as long
+ * <code>length = 1/speed</code>
+ */
+public class SpeedModifier extends IModifier {
+    public float speed;
+
+    private float delta = 0;
+
+    private float shiftedDelta = 0;
+
+    @Override
+    public void tick() {
+        float delta = 1f - this.delta;
+        this.delta = 0;
+        step(delta);
+    }
+
+    @Override
+    public void setupAnim(float tickDelta) {
+        float delta = tickDelta - this.delta; //this should stay positive
+        this.delta = tickDelta;
+        step(delta);
+    }
+
+    protected void step(float delta) {
+        delta *= speed;
+        while (delta > 1) {
+            delta -= 1;
+            tick();
+        }
+        setupAnim(delta);
+        this.shiftedDelta = delta;
+    }
+
+    @Override
+    public Vec3f get3DTransform(String modelName, TransformType type, float tickDelta, Vec3f value0) {
+        return super.get3DTransform(modelName, type, shiftedDelta, value0);
+    }
+}
