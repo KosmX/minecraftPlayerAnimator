43f712a847d7dcb3fc8909f6cf68eb4243bd7fc1
KosmX <kosmx.mc@gmail.com>
2022-08-11 20:48:37 +0200

    Fix mixins on Forge

--- minecraft/forge/src/main/java/dev/kosmx/playerAnim/impl/ForgeModInterface.java
+++ minecraft/forge/src/main/java/dev/kosmx/playerAnim/impl/ForgeModInterface.java
@@ -1,10 +1,17 @@
 package dev.kosmx.playerAnim.impl;
 
 import net.minecraftforge.fml.common.Mod;
+import org.spongepowered.asm.mixin.Mixins;
 
 @Mod("playeranimator")
 public class ForgeModInterface {
     public ForgeModInterface() {
         //Hello world!
+    }
+    static {
+        if (Helper.isBendEnabled()) {
+            //Use bend mixin(s) ONLY in bend mode
+            Mixins.addConfiguration("playerAnimator-onlyBend.mixins.json");
+        }
     }
 }
