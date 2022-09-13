3f90ff99a3b189a1a981250745409999708e9f94
KosmX <kosmx.mc@gmail.com>
2022-09-13 20:31:19 +0200

    Refactor resource loading, Fix multiple issues with deserializing

--- minecraft/fabric/src/main/java/dev/kosmx/playerAnim/impl/client/FabricClientInitializer.java
+++ minecraft/fabric/src/main/java/dev/kosmx/playerAnim/impl/client/FabricClientInitializer.java
@@ -1,6 +1,5 @@
 package dev.kosmx.playerAnim.impl.client;
 
-import dev.kosmx.playerAnim.core.data.gson.AnimationSerializing;
 import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationRegistry;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
@@ -9,10 +8,11 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.resources.ResourceManager;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FabricClientInitializer implements ClientModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("player-animator");
 
     @Override
     public void onInitializeClient() {
@@ -24,20 +24,7 @@ public class FabricClientInitializer implements ClientModInitializer {
 
             @Override
             public void onResourceManagerReload(@NotNull ResourceManager manager) {
-                PlayerAnimationRegistry.clearAnimation();
-                for (var resource: manager.listResources("player_animation", location -> location.getPath().endsWith(".json")).entrySet()) {
-                    try (var input = resource.getValue().open()) {
-
-                        //Deserialize the animation json. GeckoLib animation json can contain multiple animations.
-                        for (var animation : AnimationSerializing.deserializeAnimation(input)) {
-
-                            //Save the animation for later use.
-                            PlayerAnimationRegistry.addAnimation(new ResourceLocation(resource.getKey().getNamespace(), PlayerAnimationRegistry.serializeTextToString((String) animation.extraData.get("name"))), animation);
-                        }
-                    } catch(IOException e) {
-                        throw new RuntimeException(e);//Somehow handle invalid animations
-                    }
-                }
+                PlayerAnimationRegistry.resourceLoaderCallback(manager, LOGGER);
             }
         });
     }
--- minecraft/forge/src/main/java/dev/kosmx/playerAnim/impl/ForgeModInterface.java
+++ minecraft/forge/src/main/java/dev/kosmx/playerAnim/impl/ForgeModInterface.java
@@ -1,10 +1,15 @@
 package dev.kosmx.playerAnim.impl;
 
 import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixins;
 
 @Mod("playeranimator")
 public class ForgeModInterface {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("player-animator");
+
     public ForgeModInterface() {
     }
 
