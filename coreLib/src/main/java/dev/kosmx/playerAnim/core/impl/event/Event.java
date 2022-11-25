72a4ac15c103fff5a92c874a1358339c4d707b29
KosmX <kosmx.mc@gmail.com>
2022-11-25 15:09:15 +0100

    random api note

--- coreLib/src/main/java/dev/kosmx/playerAnim/core/impl/event/Event.java
+++ coreLib/src/main/java/dev/kosmx/playerAnim/core/impl/event/Event.java
@@ -19,6 +19,7 @@ public class Event<T> {
      * Do EVENT.invoker()./invoke(Objects...)/;
      * Only when firing the event.
      * @return the invoker
+     * @apiNote This shall <bold>only</bold> be used by the API
      */
     public final T invoker(){
         return _invoker.invoker(listeners);
