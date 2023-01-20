2769dca3a48c6c089c94f32cb80758cf829a24ee
KosmX <kosmx.mc@gmail.com>
2023-01-21 00:56:22 +0100

    Now with clamp guards(tm) - fix neck twist bug in multiplayer

--- minecraft/common/src/main/java/dev/kosmx/playerAnim/impl/animation/AnimationApplier.java
+++ minecraft/common/src/main/java/dev/kosmx/playerAnim/impl/animation/AnimationApplier.java
@@ -4,6 +4,7 @@ package dev.kosmx.playerAnim.impl.animation;
 import dev.kosmx.playerAnim.api.TransformType;
 import dev.kosmx.playerAnim.api.layered.IAnimation;
 import dev.kosmx.playerAnim.core.impl.AnimationProcessor;
+import dev.kosmx.playerAnim.core.util.MathHelper;
 import dev.kosmx.playerAnim.core.util.Pair;
 import dev.kosmx.playerAnim.core.util.Vec3f;
 import net.minecraft.client.model.geom.ModelPart;
@@ -20,7 +21,10 @@ public class AnimationApplier extends AnimationProcessor {
         part.x = pos.getX();
         part.y = pos.getY();
         part.z = pos.getZ();
-        Vec3f rot = this.get3DTransform(partName, TransformType.ROTATION, new Vec3f(part.xRot, part.yRot, part.zRot));
+        Vec3f rot = this.get3DTransform(partName, TransformType.ROTATION, new Vec3f( // clamp guards
+                MathHelper.clampToRadian(part.xRot),
+                MathHelper.clampToRadian(part.yRot),
+                MathHelper.clampToRadian(part.zRot)));
         part.setRotation(rot.getX(), rot.getY(), rot.getZ());
         if (!partName.equals("head")) {
             if (partName.equals("torso")) {
